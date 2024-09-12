7894b60f64e6d85a6c2a0930632e8ed6a31e45cd
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-09-12 20:27:39 +0200

    add pagination page feature using React-paginate and add debounce feature to search for foods to executed only after a 0.7 second  delay following the last keystroke

--- src/assets/data/foodData.js
+++ src/assets/data/foodData.js
@@ -23,11 +23,13 @@ import food_06_image_01 from "../images/bread(1).png";
 import food_06_image_02 from "../images/bread(2).png";
 import food_06_image_03 from "../images/bread(3).png";
 
+import food_07_image_01 from "../images/fried-checken.jpg";
+
 const foodsData = [
   {
     id: "01",
     title: "Chicken Burger",
-    price: 24.0,
+    price: 154.0,
     image01: food_01_image_01,
     image02: food_01_image_02,
     image03: food_01_image_03,
@@ -39,7 +41,7 @@ const foodsData = [
   {
     id: "02",
     title: "Vegetarian Pizza",
-    price: 115.0,
+    price: 105.0,
     image01: food_02_image_01,
     image02: food_02_image_02,
     image03: food_02_image_03,
@@ -51,7 +53,7 @@ const foodsData = [
   {
     id: "03",
     title: "Double Cheese Margherita",
-    price: 110.0,
+    price: 210.0,
     image01: food_03_image_01,
     image02: food_03_image_02,
     image03: food_03_image_03,
@@ -75,7 +77,7 @@ const foodsData = [
   {
     id: "05",
     title: "Cheese Burger",
-    price: 24.0,
+    price: 144.0,
     image01: food_01_image_02,
     image02: food_01_image_01,
     image03: food_01_image_03,
@@ -86,7 +88,7 @@ const foodsData = [
   {
     id: "06",
     title: "Royal Cheese Burger",
-    price: 24.0,
+    price: 134.0,
     image01: food_01_image_03,
     image02: food_01_image_01,
     image03: food_01_image_02,
@@ -98,7 +100,7 @@ const foodsData = [
   {
     id: "07",
     title: "Seafood Pizza",
-    price: 115.0,
+    price: 165.0,
     image01: food_02_image_02,
     image02: food_02_image_01,
     image03: food_02_image_03,
@@ -110,7 +112,7 @@ const foodsData = [
   {
     id: "08",
     title: "Thin Cheese Pizza",
-    price: 110.0,
+    price: 120.0,
     image01: food_03_image_02,
     image02: food_03_image_01,
     image03: food_03_image_03,
@@ -134,7 +136,7 @@ const foodsData = [
   {
     id: "10",
     title: "Classic Hamburger",
-    price: 24.0,
+    price: 124.0,
     image01: food_04_image_02,
     image02: food_04_image_01,
     image03: food_04_image_03,
@@ -146,7 +148,7 @@ const foodsData = [
   {
     id: "11",
     title: "Crunchy Bread ",
-    price: 35.0,
+    price: 65.0,
     image01: food_06_image_01,
     image02: food_06_image_02,
     image03: food_06_image_03,
@@ -158,12 +160,23 @@ const foodsData = [
   {
     id: "13",
     title: "Loaf Bread ",
-    price: 35.0,
+    price: 75.0,
     image01: food_06_image_03,
     image02: food_06_image_02,
     image03: food_06_image_03,
     category: "Bread",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+  },
+  {
+    id: "14",
+    title: "Fried Chicken",
+    price: 150.0,
+    image01: food_07_image_01,
+    image02: food_01_image_02,
+    image03: food_01_image_03,
+    category: "Burger",
+
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
   },
 ];
 
--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -1,15 +1,52 @@
+import { useEffect, useMemo, useState } from "react";
+import ReactPaginate from "react-paginate";
+import debounce from "lodash.debounce";
 import { Col, Container, Row } from "reactstrap";
 import Helmet from "../component/helmet/Helmet";
 import CommonSection from "../component/Ui/common-section/CommonSection";
 import allFoodsData from "../assets/data/foodData";
 import ProductCard from "../component/Ui/product-card/ProductCard";
-import "../styles/all-foods.css"
-import { useState } from "react";
+import "../styles/all-foods.css";
+import "../styles/paginate-page.css";
 
 function AllFood() {
   const [searchItem, setSearchItem] = useState("");
-  const [foodsdata, setFoodsData] = useState(allFoodsData)
 
+  const [pageNumber, setPageNumber] = useState(0);
+
+function handleChange(e) {
+  setSearchItem(e.target.value);
+}
+
+  const debouncedResults = useMemo(() => {
+    return debounce(handleChange, 700);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      debouncedResults(searchItem);
+      debouncedResults.cancel();
+    };
+  }, [debouncedResults, searchItem]);
+  
+
+  const searchedFood = allFoodsData.filter((food) => {
+    if (searchItem === "") return food;
+    if (food.title.toLowerCase().includes(searchItem.toLowerCase()))
+      return food;
+  });
+  const productPerPage = 10;
+  const visitedPage = pageNumber * productPerPage;
+  const displayPage = searchedFood.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+  const pageCount = Math.ceil(searchedFood.length / productPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+  
+  
   return (
     <Helmet title="All Foods">
       <CommonSection title="All Foods" />
@@ -22,8 +59,9 @@ function AllFood() {
                   type="text"
                   name="search-text"
                   placeholder="I'm looking for..."
-                  value={searchItem}
-                  onChange={e=> setSearchItem(e.target.value)}
+                  
+                  onChange={debouncedResults}
+                  id="search-result"
                 />
                 <span>
                   <i className="ri-search-line"></i>
@@ -41,14 +79,34 @@ function AllFood() {
                 </select>
               </div>
             </Col>
-            {foodsdata.filter(food => {
-              if (searchItem.value === "") return food;
-              if(food.title.toLowerCase().includes(searchItem.toLowerCase())) return food
-            }).map((food) => (
-              <Col lg="3" md="4" sm="6" xs="6" key={food.id}>
-                <ProductCard food={food} />
+            {displayPage.length > 0 ? (
+              displayPage.map((food) => (
+                <Col lg="3" md="4" sm="6" xs="6" key={food.id}>
+                  <ProductCard food={food} />
+                </Col>
+              ))
+            ) : (
+              <Col
+                lg="12"
+                md="12"
+                sm="12"
+                xs="12"
+                className="w-100 text-center "
+              >
+                <p className="not-found-msg">
+                  No results found, please try again
+                </p>
               </Col>
-            ))}
+            )}
+            <div>
+              <ReactPaginate
+                pageCount={pageCount}
+                onPageChange={changePage}
+                previousLabel="Prev"
+                nextLabel="Next"
+                containerClassName="pagination-btns"
+              />
+            </div>
           </Row>
         </Container>
       </section>
