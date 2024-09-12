a39f74ac12a0ecbc6e85083032bd941063cc87c3
Alaa Mohammed Youssef <alaamuhamed8@gamil.com>
2024-09-12 19:47:07 +0200

    fix some small bug in paginate feature and add more media responsive to all foods page

--- src/pages/AllFood.jsx
+++ src/pages/AllFood.jsx
@@ -46,20 +46,20 @@ function handleChange(e) {
     setPageNumber(selected);
   };
   
-  
+  const displayPageLenght = displayPage.length > 0
   return (
     <Helmet title="All Foods">
       <CommonSection title="All Foods" />
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6" sm="6" xs="6" className="mb-5 w-50">
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-4">
               <div className="search-box d-flex align-items-center justify-content-between ">
                 <input
+                  className="w-75"
                   type="text"
                   name="search-text"
                   placeholder="I'm looking for..."
-                  
                   onChange={debouncedResults}
                   id="search-result"
                 />
@@ -68,7 +68,7 @@ function handleChange(e) {
                 </span>
               </div>
             </Col>
-            <Col lg="6" md="6" sm="6" xs="6" className="text-end w-50">
+            <Col lg="6" md="6" sm="6" xs="12" className="text-end mb-4">
               <div className="sorting-box">
                 <select>
                   <option value="default">Default</option>
@@ -79,7 +79,7 @@ function handleChange(e) {
                 </select>
               </div>
             </Col>
-            {displayPage.length > 0 ? (
+            {displayPageLenght ? (
               displayPage.map((food) => (
                 <Col lg="3" md="4" sm="6" xs="6" key={food.id}>
                   <ProductCard food={food} />
@@ -91,22 +91,26 @@ function handleChange(e) {
                 md="12"
                 sm="12"
                 xs="12"
-                className="w-100 text-center "
+                className="w-100 text-center"
               >
                 <p className="not-found-msg">
                   No results found, please try again
                 </p>
               </Col>
             )}
-            <div>
-              <ReactPaginate
-                pageCount={pageCount}
-                onPageChange={changePage}
-                previousLabel="Prev"
-                nextLabel="Next"
-                containerClassName="pagination-btns"
-              />
-            </div>
+            {displayPageLenght ? (
+              <div>
+                <ReactPaginate
+                  pageCount={pageCount}
+                  onPageChange={changePage}
+                  previousLabel="Prev"
+                  nextLabel="Next"
+                  containerClassName="pagination-btns"
+                />
+              </div>
+            ) : (
+              ""
+            )}
           </Row>
         </Container>
       </section>
